4270ca319eeea00e11029ccac5e361897a225e89
Sander Paniago <sanderppaniago@gmail.com>
2021-05-19 17:19:55 -0400

    backend -> configurado a base do projeto.

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+
+import './database/connection';
+
+import routes from './router';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT || 3333);
