54943c1aba0294d4301287ba25c811f0992269d9
Sander Paniago <sanderppaniago@gmail.com>
2021-05-20 18:18:20 -0400

    web:retirando o token do codigo e colocando variavel de ambianete

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -17,7 +17,7 @@ export default function Map({longitude, latitude} : MapProps) {
             dragging={false}
         >
             <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FuZGVycGFuaWFnbyIsImEiOiJja2c5dThxYXAwMHJkMnJwcTNuZHA1NnRoIn0.HSHsfwXzyrYX1oo87GcXdQ`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
             />
         </MapContainer>
     )
