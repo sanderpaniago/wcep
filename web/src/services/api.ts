09ab6e75da5ac379241879fc820cbe22179fc7d1
Sander Paniago <sanderppaniago@gmail.com>
2021-05-20 20:37:15 -0400

    DevOps: configurado o ambiente de produção front: versel backend: aws ec2.

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: process.env.AMBIENT === 'dev' ? 'http://localhost:3333/' : process.env.URL_SERVER_EC2
 })
 
 export default api
